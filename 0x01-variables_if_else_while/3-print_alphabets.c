9c817407c994f2b734bd4e7aaf3fd057fc3742f0
moaaz-abdullah <moaaz200026@gmail.com>
2023-08-11 15:46:43 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+ 
+ int main(void)
+{
+	char lett;
+
+	for (lett = 'a'; lett <= 'z'; lett++)
+		putchar(lett);
+
+	for (lett = 'A'; lett <= 'Z'; lett++)
+		putchar(lett);
+
+	putchar('\n');
+
+	return (0);
+}
