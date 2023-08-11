3541f77f3c2bf2b1a4cae5039950574ddbd0e9ea
moaaz-abdullah <moaaz200026@gmail.com>
2023-08-11 15:55:23 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all numbers of base 10.
+ *
+ * Return: Always 0.
+ */
+ 
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+		printf("%d", number);
+
+	printf("\n");
+
+	return (0);
+}
