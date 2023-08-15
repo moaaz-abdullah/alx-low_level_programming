52287a7add133515e2c78b31c80a8dbc04e28e03
moaaz-abdullah <moaaz200026@gmail.com>
2023-08-15 12:13:59 -0700

    2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,20 @@
-#include"main.h"
+#include "main.h"
+
 /**
  * print_alphabet - prints the english alphabet from a-z.
- * 
+ *
  * Return: No retutn.
  */
+
 void print_alphabet(void)
 {
-	int letter;
+    int letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		_putchar(letter);
-	}
-    
-	_putchar('\n');
+    for (letter = 'a'; letter <= 'z'; letter++)
+    {
+        _putchar(letter);
+    }
+
+    _putchar('\n');
 }
+
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -2,11 +2,16 @@
 #include "main.h"
 
 /**
+ * _putchar
+ *
+ * @c: print character
+ *
  * Return: 1
  *
  */
 
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+    return (write(1, &c, 1));
 }
+
