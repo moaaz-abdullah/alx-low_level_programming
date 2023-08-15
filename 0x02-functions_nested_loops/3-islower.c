24544c4c90af3640795932504c1f9b9d94cd7442
moaaz-abdullah <138708259+moaaz-abdullah@users.noreply.github.com>
2023-08-15 23:01:55 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
 *_islower - controls if a character is in lowercase
+*Description: controls if a character is in lowercase
 *@c: character to be verified
 *Return: return 0 or 1
 */
