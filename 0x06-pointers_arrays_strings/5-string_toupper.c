7d5de0d777f1ee22fde411a44350e0f3b975dfc8
moaaz-abdullah <138708259+moaaz-abdullah@users.noreply.github.com>
2023-08-23 18:04:36 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,19 +3,18 @@
  * string_toupper - change all lowercase to uppercase
  * @n: pointer
  *
- * Return: n lower to uppercase
+ * Return: n
  */
-char *string_toupper(char *s)
+char *string_toupper(char *n)
 {
-	int i = 0;
+	int i;
 
-	while (*(s + i) != '\0')
+	i = 0;
+	while (n[i] != '\0')
 	{
-		if (*(s + i) >= 'a' && *(s + i) <= 'z')
-		{
-			*(s + i) = *(s + i) - 32;
-		}
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
 		i++;
 	}
-	return (s);
+	return (n);
 }
