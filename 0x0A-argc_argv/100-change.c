5da1e18dcf9e03a5cefe7ee3c9fdad77ec22f5c8
moaaz-abdullah <moaaz200026@gmail.com>
2023-09-01 05:53:36 -0700

    ALX

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,37 +1,46 @@
-nclude <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 /**
  * main - Prints coints left
  * @args: Paramater Entry
  * @argv: One number of argumets
- * Return: zero onOne number of argumetsr
+ * Return: zero one number of argumetsr
  */
-int main(int argc, char *argv[])
+int main(int args, char *argv[])
 {
-	int cents, ncoins = 0;
+	int  c, coins = 0;
 
-	if (argc == 1 || argc > 2)
+	if (args != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	cents = atoi(argv[1]);
-
-	while (cents > 0)
+	c = atoi(argv[1]);
+	if (c < 0)
 	{
-		if (cents >= 25)
-			cents -= 25;
-		else if (cents >= 10)
-			cents -= 10;
-		else if (cents >= 5)
-			cents -= 5;
-		else if (cents >= 2)
-			cents -= 2;
-		else if (cents >= 1)
-			cents -= 1;
-		ncoins += 1;
+		printf("0\n");
+		return (0);
 	}
-	printf("%d\n", ncoins);
+	for (; c >= 0;)
+	{
+		if (c >= 25)
+			c -= 25;
+
+		else if (c >= 10)
+			c -= 10;
+
+		else if (c >= 5)
+			c -= 5;
+
+		else if (c >= 2)
+			c -= 2;
+
+		else if (c >= 1)
+			c -= 1;
+		else
+			break;
+		coins += 1;
+	}
+	printf("%d\n", coins);
 	return (0);
 }
