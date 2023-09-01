6139d43bf22d8e0206faae86017c062a5df51bdd
moaaz-abdullah <moaaz200026@gmail.com>
2023-09-01 05:51:26 -0700

    ALX

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,37 @@
+nclude <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints coints left
+ * @args: Paramater Entry
+ * @argv: One number of argumets
+ * Return: zero onOne number of argumetsr
+ */
+int main(int argc, char *argv[])
+{
+	int cents, ncoins = 0;
+
+	if (argc == 1 || argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else if (cents >= 1)
+			cents -= 1;
+		ncoins += 1;
+	}
+	printf("%d\n", ncoins);
+	return (0);
+}
