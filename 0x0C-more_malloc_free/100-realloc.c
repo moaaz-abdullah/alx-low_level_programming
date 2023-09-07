1253b1dffadd24908cf66f41a1e305b1e4eaa4ef
Moaaz Abdullah <138708259+moaaz-abdullah@users.noreply.github.com>
2023-09-07 15:02:14 +0300

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,41 +1,65 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
-* _realloc - reallocates a memory block
-* @ptr: pointer to the memory previously allocated with a call to malloc
-* @old_size: size of ptr
-* @new_size: size of the new memory to be allocated
-*
-* Return: pointer to the address of the new memory block
-*/
+ * _memcpy - copies memory area.
+ * @dest: the memory area to be filled
+ * @src: the origin memery area
+ * @n: number of bytes to copy
+ *
+ * Return: a pointer to the memory area dest.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *p = dest;
+
+	for (; n; n--)
+		*p++ = *src++;
+
+	return (dest);
+}
+
+/**
+ * _realloc - reallocates a memory block.
+ * @ptr: pointer to the memory previously allocated.
+ * @old_size: old size of pointer
+ * @new_size: new size of pointer
+ *
+ * Return: void *
+ */
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *temp_block;
-	unsigned int i;
+	char *nptr;
 
-	if (ptr == NULL)
-	{
-		temp_block = malloc(new_size);
-		return (temp_block);
-	}
-	else if (new_size == old_size)
+	if (new_size == old_size)
 		return (ptr);
 
-	else if (new_size == 0 && ptr != NULL)
+	if ((new_size == 0) && ptr)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	else
+
+	if (!ptr)
 	{
-		temp_block = malloc(new_size);
-		if (temp_block != NULL)
-		{
-			for (i = 0; i < min(old_size, new_size); i++)
-				*((char *)temp_block + i) = *((char *) ptr + i);
-			free(ptr);
-			return (temp_block);
-		}
-		else
+		free(ptr);
+		nptr = malloc(new_size);
+		if (!nptr)
 			return (NULL);
+		return (nptr);
 	}
+
+	if (new_size > old_size)
+	{
+		nptr = malloc(new_size);
+		if (!nptr)
+			return (NULL);
+
+		_memcpy(nptr, ptr, old_size);
+		free(ptr);
+	}
+	return (nptr);
 }
